81fa6e9689c805cafc47988d8fa662436f79251e
mahmoudahmed0111 <mahmoudahmed09733@gmail.com>
2025-08-20 23:49:57 +0300

    dfverferf

--- routes/api.php
+++ routes/api.php
@@ -46,84 +46,83 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/password', [AuthController::class, 'updatePassword']);
 
 
-        // Chalets routes
-        Route::prefix('chalets')->group(function () {
-            Route::get('/', [ChaletController::class, 'index']);
-            Route::get('/{chalet}', [ChaletController::class, 'show']);
-        });
+    // Chalets routes
+    Route::prefix('chalets')->group(function () {
+        Route::get('/', [ChaletController::class, 'index']);
+        Route::get('/{chalet}', [ChaletController::class, 'show']);
+    });
 
-        // Regular Cleanings routes
-        Route::prefix('regular-cleanings')->group(function () {
-            Route::get('/', [RegularCleaningController::class, 'index']);
-            Route::post('/', [RegularCleaningController::class, 'store']);
-            Route::get('/{regularCleaning}', [RegularCleaningController::class, 'show']);
-            Route::put('/{regularCleaning}', [RegularCleaningController::class, 'update']);
-            Route::post('/{regularCleaning}/images', [RegularCleaningController::class, 'uploadImages']);
-            Route::post('/{regularCleaning}/videos', [RegularCleaningController::class, 'uploadVideos']);
-        });
+    // Regular Cleanings routes
+    Route::prefix('regular-cleanings')->group(function () {
+        Route::get('/', [RegularCleaningController::class, 'index']);
+        Route::post('/', [RegularCleaningController::class, 'store']);
+        Route::get('/{regularCleaning}', [RegularCleaningController::class, 'show']);
+        Route::put('/{regularCleaning}', [RegularCleaningController::class, 'update']);
+        Route::post('/{regularCleaning}/images', [RegularCleaningController::class, 'uploadImages']);
+        Route::post('/{regularCleaning}/videos', [RegularCleaningController::class, 'uploadVideos']);
+    });
 
-        // Deep Cleanings routes
-        Route::prefix('deep-cleanings')->group(function () {
-            Route::get('/', [DeepCleaningController::class, 'index']);
-            Route::post('/', [DeepCleaningController::class, 'store']);
-            Route::get('/{deepCleaning}', [DeepCleaningController::class, 'show']);
-            Route::put('/{deepCleaning}', [DeepCleaningController::class, 'update']);
-            Route::post('/{deepCleaning}/images', [DeepCleaningController::class, 'uploadImages']);
-            Route::post('/{deepCleaning}/videos', [DeepCleaningController::class, 'uploadVideos']);
-        });
+    // Deep Cleanings routes
+    Route::prefix('deep-cleanings')->group(function () {
+        Route::get('/', [DeepCleaningController::class, 'index']);
+        Route::post('/', [DeepCleaningController::class, 'store']);
+        Route::get('/{deepCleaning}', [DeepCleaningController::class, 'show']);
+        Route::put('/{deepCleaning}', [DeepCleaningController::class, 'update']);
+        Route::post('/{deepCleaning}/images', [DeepCleaningController::class, 'uploadImages']);
+        Route::post('/{deepCleaning}/videos', [DeepCleaningController::class, 'uploadVideos']);
+    });
 
-        // Maintenance routes
-        Route::prefix('maintenance')->group(function () {
-            Route::get('/', [MaintenanceController::class, 'index']);
-            Route::post('/', [MaintenanceController::class, 'store']);
-            Route::get('/{maintenance}', [MaintenanceController::class, 'show']);
-            Route::put('/{maintenance}', [MaintenanceController::class, 'update']);
-            Route::post('/{maintenance}/images', [MaintenanceController::class, 'uploadImages']);
-            Route::post('/{maintenance}/videos', [MaintenanceController::class, 'uploadVideos']);
-        });
+    // Maintenance routes
+    Route::prefix('maintenance')->group(function () {
+        Route::get('/', [MaintenanceController::class, 'index']);
+        Route::post('/', [MaintenanceController::class, 'store']);
+        Route::get('/{maintenance}', [MaintenanceController::class, 'show']);
+        Route::put('/{maintenance}', [MaintenanceController::class, 'update']);
+        Route::post('/{maintenance}/images', [MaintenanceController::class, 'uploadImages']);
+        Route::post('/{maintenance}/videos', [MaintenanceController::class, 'uploadVideos']);
+    });
 
-        // Pest Control routes
-        Route::prefix('pest-controls')->group(function () {
-            Route::get('/', [PestControlController::class, 'index']);
-            Route::post('/', [PestControlController::class, 'store']);
-            Route::get('/{pestControl}', [PestControlController::class, 'show']);
-            Route::put('/{pestControl}', [PestControlController::class, 'update']);
-            Route::post('/{pestControl}/images', [PestControlController::class, 'uploadImages']);
-            Route::post('/{pestControl}/videos', [PestControlController::class, 'uploadVideos']);
-        });
+    // Pest Control routes
+    Route::prefix('pest-controls')->group(function () {
+        Route::get('/', [PestControlController::class, 'index']);
+        Route::post('/', [PestControlController::class, 'store']);
+        Route::get('/{pestControl}', [PestControlController::class, 'show']);
+        Route::put('/{pestControl}', [PestControlController::class, 'update']);
+        Route::post('/{pestControl}/images', [PestControlController::class, 'uploadImages']);
+        Route::post('/{pestControl}/videos', [PestControlController::class, 'uploadVideos']);
+    });
 
-        // Damages routes
-        Route::prefix('damages')->group(function () {
-            Route::get('/', [DamageController::class, 'index']);
-            Route::post('/', [DamageController::class, 'store']);
-            Route::get('/{damage}', [DamageController::class, 'show']);
-            Route::put('/{damage}', [DamageController::class, 'update']);
-            Route::post('/{damage}/images', [DamageController::class, 'uploadImages']);
-            Route::post('/{damage}/videos', [DamageController::class, 'uploadVideos']);
-        });
+    // Damages routes
+    Route::prefix('damages')->group(function () {
+        Route::get('/', [DamageController::class, 'index']);
+        Route::post('/', [DamageController::class, 'store']);
+        Route::get('/{damage}', [DamageController::class, 'show']);
+        Route::put('/{damage}', [DamageController::class, 'update']);
+        Route::post('/{damage}/images', [DamageController::class, 'uploadImages']);
+        Route::post('/{damage}/videos', [DamageController::class, 'uploadVideos']);
+    });
 
-        // Inventory routes
-        Route::prefix('inventory')->group(function () {
-            Route::get('/', [InventoryController::class, 'index']);
-            Route::get('/{inventory}', [InventoryController::class, 'show']);
-        });
+    // Inventory routes
+    Route::prefix('inventory')->group(function () {
+        Route::get('/', [InventoryController::class, 'index']);
+        Route::get('/{inventory}', [InventoryController::class, 'show']);
+    });
 
-        // Settings routes
-        Route::prefix('settings')->group(function () {
-            Route::get('/', [SettingController::class, 'index']);
-        });
+    // Settings routes
+    Route::prefix('settings')->group(function () {
+        Route::get('/', [SettingController::class, 'index']);
+    });
 
-        // Notifications routes
-        Route::prefix('notifications')->group(function () {
-            Route::get('/', [NotificationController::class, 'index']);
-            Route::get('/stats', [NotificationController::class, 'stats']);
-            Route::get('/{notification}', [NotificationController::class, 'show']);
-            Route::put('/{notification}/read', [NotificationController::class, 'markAsRead']);
-            Route::put('/read-all', [NotificationController::class, 'markAllAsRead']);
-            Route::delete('/{notification}', [NotificationController::class, 'destroy']);
-            Route::delete('/', [NotificationController::class, 'destroyAll']);
-            Route::post('/fcm-token', [NotificationController::class, 'updateFcmToken']);
-            Route::post('/test', [NotificationController::class, 'sendTestNotification']);
-        });
-
+    // Notifications routes
+    Route::prefix('notifications')->group(function () {
+        Route::get('/', [NotificationController::class, 'index']);
+        Route::get('/stats', [NotificationController::class, 'stats']);
+        Route::get('/{notification}', [NotificationController::class, 'show']);
+        Route::put('/{notification}/read', [NotificationController::class, 'markAsRead']);
+        Route::put('/read-all', [NotificationController::class, 'markAllAsRead']);
+        Route::delete('/{notification}', [NotificationController::class, 'destroy']);
+        Route::delete('/', [NotificationController::class, 'destroyAll']);
+        Route::post('/fcm-token', [NotificationController::class, 'updateFcmToken']);
+        Route::post('/test', [NotificationController::class, 'sendTestNotification']);
+    });
 });
